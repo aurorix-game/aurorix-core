247c6953f7e81efb018abe96ec25291a1b6260d6
gabriel-roque <gabrielrms.dll@gmail.com>
2024-01-03 19:29:17 -0300

    📦 feat: update props mopy

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 export * from './character.model';
 export * from './elements.model';
+export * from './mopy.model';
 export * from './type-values.model';
 export * from './user-character.model';
 export * from './user.model';
--- src/models/mopy.model.ts
+++ src/models/mopy.model.ts
@@ -24,14 +24,47 @@ export namespace Mopy {
     type: TypeAttributes;
     type_value: TypeValues;
     value: number;
-    style: AttributeStyle;
+  };
+
+  export type LevelAttribute = {
+    value: number;
+    type_value: TypeValues;
+  };
+
+  export type HealthAttribute = {
+    max_value: number;
+    current_value: number;
+    type_value: TypeValues;
+  };
+
+  export type AttackAttribute = {
+    value: number;
+    type_value: TypeValues;
+  };
+
+  export type DefenseAttribute = {
+    value: number;
+    type_value: TypeValues;
+  };
+
+  export type ExperienceAttribute = {
+    max_value: number;
+    current_value: number;
+    type_value: TypeValues;
   };
 
   export type Model = {
     id: string;
     alias_name: string;
     sprite: Sprite;
-    attributes: Attribute[];
+    att: {
+      level: LevelAttribute;
+      health: HealthAttribute;
+      attack: AttackAttribute;
+      defense: DefenseAttribute;
+      elements: Elements[];
+      xp: ExperienceAttribute;
+    };
   };
 
   export type Ability = {
